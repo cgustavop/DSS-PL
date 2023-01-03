fbc7938c4b3017a8a675a2e1edea3e66047d9b70
gustavo <carlosgsp39@gmail.com>
2023-01-03 17:23:22 +0000

    Add PilotoDAO

--- EntregaFinal/src/SubPilotos/PilotoDAO.java
+++ EntregaFinal/src/SubPilotos/PilotoDAO.java
@@ -1,10 +1,37 @@
 package EntregaFinal.src.SubPilotos;
+import EntregaFinal.src.data.DAOconfig;
 
+import java.sql.*;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class PilotoDAO implements Map<String,Piloto> {
+
+	private static PilotoDAO singleton = null;
+
+	private PilotoDAO(){
+		try (Connection conn = DriverManager.getConnection(DAOconfig.URL, DAOconfig.USERNAME, DAOconfig.PASSWORD);
+             Statement stm = conn.createStatement()) {
+            String sql = "CREATE TABLE IF NOT EXISTS pilotos (" +
+                    "Nome varchar(45) NOT NULL PRIMARY KEY," +
+                    "Cts float DEFAULT 0," +
+					"Sva float DEFAULT 0)";
+            stm.executeUpdate(sql);
+        } catch (SQLException e) {
+            // Erro a criar tabela...
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+	}
+
+	public static PilotoDAO getInstance() {
+        if (PilotoDAO.singleton == null) {
+            PilotoDAO.singleton = new PilotoDAO();
+        }
+        return PilotoDAO.singleton;
+    }
 
 	public int hashCode() {
 		int lHashCode = 0;
@@ -16,47 +43,120 @@ public class PilotoDAO implements Map<String,Piloto> {
 
 	@Override
 	public int size() {
-		return 0;
+		int i = 0;
+        try (Connection conn = DriverManager.getConnection(DAOconfig.URL, DAOconfig.USERNAME, DAOconfig.PASSWORD);
+             Statement stm = conn.createStatement();
+             ResultSet rs = stm.executeQuery("SELECT count(*) FROM pilotos")) {
+            if(rs.next()) {
+                i = rs.getInt(1);
+            }
+        }
+        catch (Exception e) {
+            // Erro a criar tabela...
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return i;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return false;
+		return this.size() == 0;
 	}
 
 	@Override
 	public boolean containsKey(Object key) {
-		return false;
+		boolean r;
+        try (Connection conn = DriverManager.getConnection(DAOconfig.URL, DAOconfig.USERNAME, DAOconfig.PASSWORD);
+             Statement stm = conn.createStatement();
+             ResultSet rs =
+                     stm.executeQuery("SELECT Nome FROM pilotos WHERE Nome='"+key.toString()+"'")) {
+             r = rs.next();
+        } catch (SQLException e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return r;
 	}
 
 	@Override
 	public boolean containsValue(Object value) {
-		return false;
+		Piloto a = (Piloto) value;
+        return this.containsKey(a.get_nome());
 	}
 
 	@Override
 	public Piloto get(Object key) {
-		return null;
+		Piloto a = null;
+        try (Connection conn = DriverManager.getConnection(DAOconfig.URL, DAOconfig.USERNAME, DAOconfig.PASSWORD);
+             Statement stm = conn.createStatement();
+             ResultSet rs = stm.executeQuery("SELECT * FROM pilotos WHERE Nome='"+key+"'")) {
+            if (rs.next()) {  // A chave existe na tabela
+                a = new Piloto(rs.getString("Nome"),
+							Float.parseFloat(rs.getString("Cts")),
+							Float.parseFloat(rs.getString("Sva")));
+            }
+        } catch (SQLException e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return a;
 	}
 
 	@Override
 	public Piloto put(String key, Piloto value) {
-		return null;
+		Piloto res = null;
+        try (Connection conn = DriverManager.getConnection(DAOconfig.URL, DAOconfig.USERNAME, DAOconfig.PASSWORD);
+             Statement stm = conn.createStatement()) {
+            try(PreparedStatement pstm = conn.prepareStatement("INSERT INTO pilotos(Nome,Cts,Sva)" + "VALUES (?,?,?)")) {
+                pstm.setString(1,value.get_nome());
+                pstm.setString(2,String.valueOf(value.get_cts()));
+                pstm.setString(3,String.valueOf(value.get_sva()));
+                pstm.execute();
+            }
+        } catch (SQLException e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return res;
 	}
 
 	@Override
 	public Piloto remove(Object key) {
-		return null;
+		Piloto a = this.get(key);
+        try (Connection conn = DriverManager.getConnection(DAOconfig.URL, DAOconfig.USERNAME, DAOconfig.PASSWORD);
+             Statement stm = conn.createStatement()){
+            // apagar o aluno
+            stm.executeUpdate("DELETE FROM pilotos WHERE Nome='"+key+"'");
+        } catch (Exception e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return a;
 	}
 
 	@Override
 	public void putAll(Map<? extends String, ? extends Piloto> m) {
-
+			for(Piloto a : m.values()) {
+				this.put(a.get_nome(), a);
+			}
 	}
 
 	@Override
 	public void clear() {
-
+		try (Connection conn = DriverManager.getConnection(DAOconfig.URL, DAOconfig.USERNAME, DAOconfig.PASSWORD);
+             Statement stm = conn.createStatement()) {
+            stm.executeUpdate("UPDATE pilotos SET Piloto=NULL");
+            stm.executeUpdate("TRUNCATE pilotos");
+        } catch (SQLException e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
 	}
 
 	@Override
@@ -66,7 +166,21 @@ public class PilotoDAO implements Map<String,Piloto> {
 
 	@Override
 	public Collection<Piloto> values() {
-		return null;
+		Collection<Piloto> res = new HashSet<>();
+        try (Connection conn = DriverManager.getConnection(DAOconfig.URL, DAOconfig.USERNAME, DAOconfig.PASSWORD);
+             Statement stm = conn.createStatement();
+             ResultSet rs = stm.executeQuery("SELECT Nome FROM pilotos")) { // ResultSet com os nomes de todos os circuitos
+            while (rs.next()) {
+                String idt = rs.getString("Nome"); // Obtemos um nome de circuito do ResultSet
+                Piloto a = this.get(idt);                    // Utilizamos o get para construir os circuitos
+                res.add(a);                                 // Adiciona o circuito ao resultado.
+            }
+        } catch (Exception e) {
+            // Database error!
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+        return res;
 	}
 
 	@Override
